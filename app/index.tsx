0bf466dc4404817f533902c8da6ca1b183c35429
Dinushan-S <selvandinu18@gmail.com>
2025-05-10 00:38:37 +0530

    feat: integrate NativeWind for styling and set up Tailwind CSS configuration
    - Added NativeWind and Tailwind CSS dependencies in package.json
    - Removed reset-project script as it is no longer needed
    - Updated TypeScript configuration to include nativewind-env.d.ts
    - Created global CSS file for Tailwind directives
    - Implemented main index.tsx file with Tailwind classes for styling
    - Configured Babel to use NativeWind presets
    - Set up Metro bundler configuration to include global CSS
    - Added TypeScript definitions for NativeWind
    - Established Tailwind CSS configuration with content paths

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-white dark:bg-black"
+      >
+      <Text className="text-red-500">WEKUME</Text>
+    </View>
+  );
+}
